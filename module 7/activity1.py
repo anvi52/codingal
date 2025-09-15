f2d8f6e13e433186a4d0140feb0c91782613f3e3
anvi52 <udit987.gmail.com>
2025-09-15 19:36:41 +0530

    submit

---
+++ module 7/activity1.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("hello, world!")
+
+# printig my name
+print("My name is Alex.")
+
+# printing a fun fact about myself
+print("i love playing soccer.")
+
+ # performing and printing a simple calculation
+print("the result of 3 + 5 is:", 3 + 5)
---
+++ module 7/lesson 1/activity2.py
@@ -0,0 +1,13 @@
+#print a sentence
+print("welcome to the world of programming!")
+
+#print a number
+print(4)
+
+#next line after print
+print("hello python /n")
+
+#print all the keywords present in python
+print("keywords in python are...")
+import keyword
+print(keyword.kwlist)
