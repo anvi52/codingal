0bcd30720a26609ab74f31738a00e7cd5e292ecd
anvi52 <udit987.gmail.com>
2025-10-08 19:30:17 +0530

    submit

---
+++ module 7/lesson 3/activity4.py
@@ -0,0 +1,11 @@
+import datetime
+# using now() to get current time
+current_time = datetime.datetime.now()
+
+# printing value of now
+print("time now at greenwich meridian is :", end = "")
+print(current_time)
+
+# print calender of year 2023
+import calendar
+print("\n", calendar.calendar(2023))
