2ecf8f3a446f9ad0d7d338b8b8f17a57d55bc20b
anvi52 <udit987.gmail.com>
2025-10-15 17:26:20 +0530

    submit

--- module 7/lesson 2/AC.py
+++ module 7/lesson 2/AC.py
@@ -2,5 +2,11 @@ y = int(input("enter the value of y:"))
 x = int(input("enter the value of x:"))
 z = int(input("enter the value of z:"))
 
-order = (z , x , y)
-print("the ascending order is:", order)
+nums = [x , y , z]
+nums.sort()
+
+x, y, z = nums
+print("values in ascending order")
+print("x=", x)
+print("y=", y)
+print("z=", z)
